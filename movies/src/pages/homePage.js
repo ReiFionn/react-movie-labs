f0bfec8295d23cd7ba11783e0c8cad29f7c58d8d
reifionn <20101977@mail.wit.ie>
2024-10-11 17:22:59 +0100

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -23,18 +23,25 @@ const HomePage = (props) => {
         else setGenreFilter(value);
     };
 
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
-        return json.results;
-      })
-      .then((movies) => {
-        setMovies(movies);
-      });
+    const addToFavorites = (movieId) => {
+      const updatedMovies = movies.map((m) =>
+        m.id === movieId ? { ...m, favorite: true } : m
+      );
+      setMovies(updatedMovies);
+    };
+
+    useEffect(() => {
+      fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+      )
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json);
+          return json.results;
+        })
+        .then((movies) => {
+          setMovies(movies);
+        });
   }, []);
 
   return (
@@ -50,7 +57,7 @@ const HomePage = (props) => {
             genreFilter={genreFilter}
             />
         </Grid>
-        <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
